5cfd622ce719f1a21a8c6dcc4d4400e14f44b2f9
이원찬 <dnjscks4257@naver.com>
2024-02-28 22:12:45 +0900

    [design] WhiteShadowBox 디자인 변경

--- src/components/WhiteShadowBox.jsx
+++ src/components/WhiteShadowBox.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function WhiteShadowBox(props) {
     return (
         <div
-            className={"shadow-[inset_0px_0px_12px_rgba(0,0,0,0.8)] p-8 bg-white rounded-3xl w-full flex flex-col "
+            className={"shadow-[inset_0px_4px_2px_rgba(0,0,0,0.3),0px_4px_4px_rgba(0,0,0,0.3)] p-8 bg-white rounded-3xl w-full flex flex-col "
                 + props.className
-        }>
+            }>
             {props.children}
         </div>
     );
